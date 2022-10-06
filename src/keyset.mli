00cb96c02bea1f3a3c680b795367435fb65d9937
Richard Davison <richard.neil.davison@gmail.com>
2022-10-05 23:38:53 -0400

    Clean up clean up everybody do your share...

--- src/keyset.mli
+++ src/keyset.mli
@@ -2,17 +2,17 @@ open! Import
 
 type t = (int * int) * Key.t Incr.t
 
-val incr : t list -> Key.t list Incr.t
-val incr' : (t * t) list -> (Key.t * Key.t) list Incr.t
-val dedup : t list -> t list
-val dedup' : (t * t) list -> (t * t) list
-val unique_fingers' : (t * t) list -> (t * t) list
-val symmetric' : (t * t) list -> (t * t) list
-val pairs : t list -> (t * t) list
 val make : rows:int list -> cols:int list -> t list
-val columns : int list -> t list
-val rows : int list -> t list
-val hand : Hand.t -> t list
 val hf : Hf.t -> t list
 val hr : Hr.t -> t list
+val hand : Hand.t -> t list
 val finger : Finger.t -> t list
+val rows : int list -> t list
+val columns : int list -> t list
+val pairs : t list -> (t * t) list
+val dedup : t list -> t list
+val dedup2 : (t * t) list -> (t * t) list
+val unique_fingers2 : (t * t) list -> (t * t) list
+val symmetric2 : (t * t) list -> (t * t) list
+val incr : t list -> Key.t list Incr.t
+val incr2 : (t * t) list -> (Key.t * Key.t) list Incr.t
