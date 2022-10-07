7fcdfa3b59f82d4ca4d794c15e89e651b71e61d8
Richard Davison <richardneildavison@gmail.com>
2022-10-07 01:34:33 -0400

    Delete hr module

--- src/.hr.mli
+++
@@ -1,9 +0,0 @@
-open! Import
-
-type t = Hand.t * int [@@deriving sexp, compare, hash, equal]
-
-include Comparable.S with type t := t
-include Hashable.S with type t := t
-
-val all : t list
-val to_string : t -> string
