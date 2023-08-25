8e4a34d04fb5bd601c55103e94c101541453a927
Richard Davison <richardneildavison@gmail.com>
2023-08-25 14:11:22 -0400

    Redo scoring

--- src/score.mli
+++ src/score.mli
@@ -3,43 +3,34 @@ open! Import
 type info =
   { unweighted : float
   ; weighted : float
-  ; final : float
   }
 [@@deriving sexp_of]
 
 type t =
-  { usage : info
-  ; sfb : info
-  ; sfs : info
-  ; speed : info
-  ; inrowlls : info
-  ; outrowlls : info
-  ; scissors : float
-  ; lsb : float
-  ; slaps : float
+  { usage : info option
+  ; sfb : info option
+  ; sfs : info option
+  ; speed : info option
+  ; inrowlls : info option
+  ; outrowlls : info option
+  ; scissors : info option
+  ; lsb : info option
+  ; slaps : info option
   }
 [@@deriving sexp_of]
 
-type config =
-  { usage : float Hand_finger.Map.t -> Hand_finger.t -> float -> float
-  ; aggregate_usage :
-      float Hand_finger.Map.t -> unweighted:float -> weighted:float -> float
-  ; sfb : float Hand_finger.Map.t -> Hand_finger.t -> float -> float
-  ; aggregate_sfb : float Hand_finger.Map.t -> unweighted:float -> weighted:float -> float
-  ; sfs : float Hand_finger.Map.t -> Hand_finger.t -> float -> float
-  ; aggregate_sfs : float Hand_finger.Map.t -> unweighted:float -> weighted:float -> float
-  ; speed : float Hand_finger.Map.t -> Hand_finger.t -> float -> float
-  ; aggregate_speed :
-      float Hand_finger.Map.t -> unweighted:float -> weighted:float -> float
-  ; inrowlls : float Hand.Map.t -> Hand.t -> float -> float
-  ; aggregate_inrowlls : float Hand.Map.t -> unweighted:float -> weighted:float -> float
-  ; outrowlls : float Hand.Map.t -> Hand.t -> float -> float
-  ; aggregate_outrowlls : float Hand.Map.t -> unweighted:float -> weighted:float -> float
-  ; scissors : float -> float
-  ; lsb : float -> float
-  ; slaps : float -> float
-  }
+val make
+  :  ?usage:(float Hand_finger.Map.t -> info)
+  -> ?sfb:(float Hand_finger.Map.t -> info)
+  -> ?sfs:(float Hand_finger.Map.t -> info)
+  -> ?speed:(float Hand_finger.Map.t -> info)
+  -> ?inrowlls:(float Hand.Map.t -> info)
+  -> ?outrowlls:(float Hand.Map.t -> info)
+  -> ?scissors:(float -> info)
+  -> ?lsb:(float -> info)
+  -> ?slaps:(float -> info)
+  -> Stats.t
+  -> t Incr.t
 
-val make : Stats.t -> config:config -> t Incr.t
 val final_sum : t Incr.t -> float Incr.t
-val default_config : config
+val default_config : Stats.t -> t Incr.t
