c21f402f1d45aa16323761f2bd4f64247408dd64
Richard Davison <richardneildavison@gmail.com>
2023-08-26 20:33:45 -0400

    Multicorify

--- src/annealing.ml
+++ src/annealing.ml
@@ -1,85 +1,113 @@
 open! Import
 
-let acceptance_probability old_cost new_cost temperature =
-  let ( < ) = Float.( < ) in
-  let res =
-    if new_cost < old_cost then 1. else Float.exp ((old_cost -. new_cost) /. temperature)
-  in
-  (* if new_cost < old_cost
-     then
-     printf
-     "%.20f\t%.20f\t%.12f\n%!"
-     new_cost
-     ((old_cost -. new_cost) /. temperature)
-     temperature; *)
-  res
-;;
+module type S = sig
+  module Incr : Incremental.S
+  module Layout : Layout.S
+  module Stats : Stats.S
+  module Score : Score.S
 
-let simulated_annealing
-  ~objective_function
-  ~make_next_solution
-  ~(initial_solution : Layout.save_state)
-  ~initial_temperature
-  ~cooling_rate
-  ~num_iterations
-  =
-  let current_solution = ref initial_solution in
-  let best_solution = ref initial_solution in
-  let current_cost = ref (objective_function !current_solution) in
-  let best_cost = ref !current_cost in
-  let temperature = ref initial_temperature in
-  let ( > ) = Float.( > ) in
-  let ( < ) = Float.( < ) in
-  for _ = 0 to num_iterations - 1 do
-    let new_solution = make_next_solution !current_solution in
-    let new_cost = objective_function new_solution in
-    if acceptance_probability !current_cost new_cost !temperature > Random.float 1.
-    then (
-      current_solution := new_solution;
-      current_cost := new_cost);
-    if new_cost < !best_cost
-    then (
-      best_solution := new_solution;
-      best_cost := new_cost);
-    temperature := !temperature *. cooling_rate
-  done;
-  !best_solution, !best_cost
-;;
+  val run
+    :  Layout.t
+    -> corpus:Corpus.t
+    -> score:(Stats.t -> Score.t Incr.t)
+    -> float * Stats.t * Score.t Incr.t * Layout.save_state
+end
 
-let run layout ~corpus ~score =
-  let stats = Stats.make layout corpus in
-  let score = score stats in
-  let final_sum = Score.final_sum score in
-  let observer = Incr.observe final_sum in
-  let make_next_solution save_state =
-    Layout.load layout save_state;
-    let swaps = ref [] in
-    while List.is_empty !swaps do
-      let a, b = Random.int layout.num_keys, Random.int layout.num_keys in
-      swaps := Layout.swaps layout a b
+module Make
+    (Incr : Incremental.S)
+    (Layout : Layout.S with module Incr = Incr)
+    (Stats : Stats.S with module Incr = Incr and module Layout = Layout)
+    (Score : Score.S
+               with module Incr = Incr
+                and module Layout = Layout
+                and module Stats = Stats) =
+struct
+  module Incr = Incr
+  module Layout = Layout
+  module Stats = Stats
+  module Score = Score
+
+  let acceptance_probability old_cost new_cost temperature =
+    let ( < ) = Float.( < ) in
+    let res =
+      if new_cost < old_cost then 1. else Float.exp ((old_cost -. new_cost) /. temperature)
+    in
+    if new_cost < old_cost
+    then
+      printf
+        "%.20f\t%.20f\t%.12f\n%!"
+        new_cost
+        ((old_cost -. new_cost) /. temperature)
+        temperature;
+    res
+  ;;
+
+  let simulated_annealing
+    ~objective_function
+    ~make_next_solution
+    ~(initial_solution : Layout.save_state)
+    ~initial_temperature
+    ~cooling_rate
+    ~num_iterations
+    =
+    let current_solution = ref initial_solution in
+    let best_solution = ref initial_solution in
+    let current_cost = ref (objective_function !current_solution) in
+    let best_cost = ref !current_cost in
+    let temperature = ref initial_temperature in
+    let ( > ) = Float.( > ) in
+    let ( < ) = Float.( < ) in
+    for _ = 0 to num_iterations - 1 do
+      let new_solution = make_next_solution !current_solution in
+      let new_cost = objective_function new_solution in
+      if acceptance_probability !current_cost new_cost !temperature > Random.float 1.
+      then (
+        current_solution := new_solution;
+        current_cost := new_cost);
+      if new_cost < !best_cost
+      then (
+        best_solution := new_solution;
+        best_cost := new_cost);
+      temperature := !temperature *. cooling_rate
     done;
-    Layout.swap !swaps;
-    Layout.save layout
-  in
-  let objective_function (save_state : Layout.save_state) =
-    Layout.load layout save_state;
-    Incr.stabilize ();
-    let score = Incr.Observer.value_exn observer in
-    score
-  in
-  let initial_solution = Layout.save layout in
-  let initial_temperature = 100.0 in
-  let cooling_rate = 0.99997 in
-  let num_iterations = 1_000_000 in
-  (* let num_iterations = 0 in *)
-  let best_solution, best_cost =
-    simulated_annealing
-      ~objective_function
-      ~make_next_solution
-      ~initial_solution
-      ~initial_temperature
-      ~cooling_rate
-      ~num_iterations
-  in
-  best_cost, stats, score, best_solution
-;;
+    !best_solution, !best_cost
+  ;;
+
+  let run layout ~corpus ~score =
+    let stats = Stats.make layout corpus in
+    let score = score stats in
+    let final_sum = Score.final_sum score in
+    let observer = Incr.observe final_sum in
+    let make_next_solution save_state =
+      Layout.load layout save_state;
+      let swaps = ref [] in
+      while List.is_empty !swaps do
+        let a, b = Random.int layout.num_keys, Random.int layout.num_keys in
+        swaps := Layout.swaps layout a b
+      done;
+      Layout.swap !swaps;
+      Layout.save layout
+    in
+    let objective_function (save_state : Layout.save_state) =
+      Layout.load layout save_state;
+      Incr.stabilize ();
+      let score = Incr.Observer.value_exn observer in
+      score
+    in
+    let initial_solution = Layout.save layout in
+    let initial_temperature = 100.0 in
+    let cooling_rate = 0.99997 in
+    let num_iterations = 1_000_000 in
+    (* let num_iterations = 0 in *)
+    let best_solution, best_cost =
+      simulated_annealing
+        ~objective_function
+        ~make_next_solution
+        ~initial_solution
+        ~initial_temperature
+        ~cooling_rate
+        ~num_iterations
+    in
+    best_cost, stats, score, best_solution
+  ;;
+end
--- src/annealing.mli
+++ src/annealing.mli
@@ -1,7 +1,28 @@
 open! Import
 
-val run
-  :  Layout.t
-  -> corpus:Corpus.t
-  -> score:(Stats.t -> Score.t Incr.t)
-  -> float * Stats.t * Score.t Incr.t * Layout.save_state
+module type S = sig
+  module Incr : Incremental.S
+  module Layout : Layout.S
+  module Stats : Stats.S
+  module Score : Score.S
+
+  val run
+    :  Layout.t
+    -> corpus:Corpus.t
+    -> score:(Stats.t -> Score.t Incr.t)
+    -> float * Stats.t * Score.t Incr.t * Layout.save_state
+end
+
+module Make
+    (Incr : Incremental.S)
+    (Layout : Layout.S with module Incr = Incr)
+    (Stats : Stats.S with module Incr = Incr and module Layout = Layout)
+    (Score : Score.S
+               with module Incr = Incr
+                and module Layout = Layout
+                and module Stats = Stats) :
+  S
+    with module Incr = Incr
+     and module Layout = Layout
+     and module Stats = Stats
+     and module Score = Score
