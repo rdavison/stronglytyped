44180f777922b3fdd018c9e3234904b626532540
Richard Davison <richard.neil.davison@gmail.com>
2025-09-08 18:01:14 +0000

    Move Bigram_data

---
+++ analysis/bigram_data.mli
@@ -0,0 +1,12 @@
+open! Core
+open! Bonsai
+
+type info =
+  { bigram : string
+  ; dist : float
+  ; freqs : float Corpus.bigrams
+  }
+
+type t = info Key.Id.Pair.Map.t
+
+val make : Keyboard.t Bonsai.t -> Corpus.t Bonsai.t -> Bonsai.graph -> t Bonsai.t
