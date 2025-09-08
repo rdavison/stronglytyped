84a3ca81de987af54c53040a6f1a232897b0ba75
Richard Davison <richard.neil.davison@gmail.com>
2025-09-08 18:15:24 +0000

    Update type

--- analysis/corpus.mli
+++ analysis/corpus.mli
@@ -1,33 +1,37 @@
 open! Core
 
-type 'a maps =
-  { a : 'a Char.Map.t
-  ; ab : 'a String.Map.t
-  ; aba : 'a String.Map.t
-  ; abxab : 'a String.Map.t
-  ; abxba : 'a String.Map.t
-  ; abc : 'a String.Map.t
-  ; abcd : 'a String.Map.t
-  ; axc : 'a String.Map.t
-  ; vbcv : 'a String.Map.t
-  }
-[@@deriving sexp, equal]
+module Maps : sig
+  type 'a t =
+    { a : 'a Char.Map.t
+    ; ab : 'a String.Map.t
+    ; aba : 'a String.Map.t
+    ; abxab : 'a String.Map.t
+    ; abxba : 'a String.Map.t
+    ; abc : 'a String.Map.t
+    ; abcd : 'a String.Map.t
+    ; axc : 'a String.Map.t
+    ; vbcv : 'a String.Map.t
+    }
+  [@@deriving sexp, equal]
+end
 
-type 'a bigrams =
-  { ab : 'a
-  ; aba : 'a
-  ; axc : 'a
-  ; vbcv : 'a
-  }
-[@@deriving sexp, equal]
+module Bigrams : sig
+  type 'a t =
+    { ab : 'a
+    ; aba : 'a
+    ; axc : 'a
+    ; vbcv : 'a
+    }
+  [@@deriving sexp, equal]
+end
 
 type t =
-  { count : int maps
-  ; freq : float maps
+  { count : int Maps.t
+  ; freq : float Maps.t
   }
 [@@deriving sexp, equal]
 
 val empty : t
 val of_string : string -> t
-val bigrams : t -> string -> float bigrams
+val bigrams : t -> string -> float Bigrams.t
 val foo : t
