0730043fffeb479c28aeebb5cfc869ef45014f82
Richard Davison <richard.neil.davison@gmail.com>
2025-09-14 06:02:07 +0000

    Extract color utils

--- web/keyboard.ml
+++ web/keyboard.ml
@@ -5,14 +5,6 @@ module Key = Analysis.Key
 module Keycode = Analysis.Keycode
 module Keyboard = Analysis.Keyboard
 
-let convert_hex_to_rgb =
-  let f s = Int.of_string ("0x" ^ s) in
-  fun (`Hex s) ->
-    let s = String.lowercase s in
-    let r = String.slice s 1 3 in
-    let g = String.slice s 3 5 in
-    let b = String.slice s 5 7 in
-    `RGB (f r, f g, f b)
 ;;
 
 let component keyboard (corpus : Analysis.Corpus.t Bonsai.t) graph =
