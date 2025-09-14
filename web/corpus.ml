efb5de8b2d8bac5d6ba57679dbcacc20a959be3d
Docker <docker@example.com>
2025-09-14 03:41:51 +0000

    Fix corpus

--- web/corpus.ml
+++ web/corpus.ml
@@ -1,7 +1,7 @@
 open! Core
 open! Bonsai_web
 open! Bonsai.Let_syntax
-module Form = Bonsai_web_ui_form
+module Form = Bonsai_web_ui_form.With_manual_view
 module Corpus = Stronglytyped_analysis.Corpus
 module Key = Stronglytyped_analysis.Key
 
@@ -9,45 +9,56 @@ module T = struct
   type t =
     | Foo
     | Custom
-  [@@deriving sexp, equal, enumerate]
+  [@@deriving sexp, equal, compare, enumerate]
 end
 
-include T
+let better_component graph =
+  let dropdown, dropdown_vdom =
+    let form = Form.Elements.Dropdown.enumerable (module T) graph in
+    let value =
+      let%arr form = form in
+      form |> Form.value_or_default ~default:T.Foo
+    in
+    let vdom =
+      let%arr form = form in
+      Form.view form
+    in
+    value, vdom
+  in
+  let textarea, textarea_vdom =
+    let extra_attrs =
+      let%map dropdown = dropdown in
+      match dropdown with
+      | Foo -> [ Vdom.Attr.disabled ]
+      | Custom -> []
+    in
+    let form = Form.Elements.Textarea.string ~extra_attrs () graph in
+    let value =
+      let%arr form = form in
+      Form.value_or_default ~default:"" form
+    in
+    let vdom =
+      let%arr form = form in
+      Form.view form
+    in
+    value, vdom
+  in
+  let corpus =
+    let%arr dropdown = dropdown
+    and textarea = textarea in
+    match dropdown with
+    | Custom -> Corpus.of_string textarea
+    | Foo -> Corpus.foo
+  in
+  let view =
+    let%arr dropdown_vdom = dropdown_vdom
+    and textarea_vdom = textarea_vdom in
+    Vdom.Node.div [ dropdown_vdom; textarea_vdom ]
+  in
+  corpus, view
+;;
 
-(* let better_component = *)
-(*   let%sub dropdown, dropdown_vdom = *)
-(*     let%sub form = Form.Elements.Dropdown.list (module T) (Value.return T.all) in *)
-(*     let%arr form = form in *)
-(*     let value = form |> Form.value_or_default ~default:T.Foo in *)
-(*     let vdom = Form.view_as_vdom ~editable:`Yes_always form in *)
-(*     value, vdom *)
-(*   in *)
-(*   let%sub textarea, textarea_vdom = *)
-(*     let extra_attrs = *)
-(*       let%map dropdown = dropdown in *)
-(*       match dropdown with *)
-(*       | Foo -> [ Vdom.Attr.disabled ] *)
-(*       | Custom -> [] *)
-(*     in *)
-(*     let%sub form = Form.Elements.Textarea.string ~extra_attrs () in *)
-(*     let%arr form = form in *)
-(*     let value = form |> Form.value_or_default ~default:"" in *)
-(*     let vdom = form |> Form.view_as_vdom in *)
-(*     value, vdom *)
-(*   in *)
-(*   let%arr dropdown = dropdown *)
-(*   and textarea = textarea *)
-(*   and dropdown_vdom = dropdown_vdom *)
-(*   and textarea_vdom = textarea_vdom in *)
-(*   let corpus = *)
-(*     match dropdown with *)
-(*     | Custom -> Corpus.of_string textarea *)
-(*     | Foo -> Corpus.foo *)
-(*   in *)
-(*   corpus, Vdom.Node.div [ dropdown_vdom; textarea_vdom ] *)
-(* ;; *)
-
-let simple_component graph =
+let _simple_component graph =
   let corpus, set_corpus = Bonsai.state Corpus.empty graph in
   let vdom =
     let%arr set_corpus = set_corpus in
@@ -59,4 +70,4 @@ let simple_component graph =
   corpus, vdom
 ;;
 
-let component = simple_component
+let component = better_component
