3a0b7916cadc8e4acc4a79951dc0cb1013021107
Docker <docker@example.com>
2025-09-15 08:23:25 +0000

    refactor

--- web/nav.ml
+++ web/nav.ml
@@ -2,6 +2,57 @@ open! Core
 open! Bonsai_web
 open! Bonsai.Let_syntax
 
+let brute_force_indexes_button ~keyboard_inject ~keyboard graph =
+  let%arr effects =
+    let%arr keyboard_inject = keyboard_inject
+    and indexes_swaps_for_brute_forcing =
+      let%arr x =
+        Bonsai.assoc
+          (module Analysis.Hand)
+          (Bonsai.Map.index_by
+             keyboard
+             ~comparator:(module Analysis.Hand)
+             ~index:(fun (key : Analysis.Key.t) ->
+               if Analysis.Finger.equal key.finger `i then Some key.hand else None)
+             graph)
+          graph
+          ~f:(fun _ data graph ->
+            let%arr keys = Bonsai.Map.keys data graph in
+            let keys = Set.to_list keys in
+            let visited = ref Analysis.Key.Id.Pair.Set.empty in
+            List.cartesian_product keys keys
+            |> List.filter_map ~f:(fun (k1, k2) ->
+              if Key.Id.equal k1 k2
+              then None
+              else (
+                let k12 = k1, k2 in
+                let k21 = k2, k1 in
+                let curr = !visited in
+                match Set.mem curr k12, Set.mem curr k21 with
+                | false, false ->
+                  visited := Set.add !visited k12;
+                  visited := Set.add !visited k21;
+                  Some k12
+                | _, _ -> None))
+            |> fun l ->
+            List.take l 1
+            |> List.map ~f:(fun x ->
+              print_s ([%sexp_of: Key.Id.t * Key.Id.t] x);
+              x))
+      in
+      List.concat (Map.data x)
+    in
+    let effects =
+      List.map indexes_swaps_for_brute_forcing ~f:(fun swap ->
+        keyboard_inject (Analysis.Keyboard.Action.Swap swap))
+    in
+    Ui_effect.all_unit effects
+  in
+  Vdom.Node.button
+    ~attrs:[ Vdom.Attr.on_click (fun _event -> effects) ]
+    [ Vdom.Node.text "Brute Force indexes" ]
+;;
+
 let component
       ~same_finger_controls
       ~keyboard
@@ -24,54 +75,7 @@ let component
          Bonsai.return ()
   in
   let brute_force_indexes_button =
-    let%arr effects =
-      let%arr keyboard_inject = keyboard_inject
-      and indexes_swaps_for_brute_forcing =
-        let%arr x =
-          Bonsai.assoc
-            (module Analysis.Hand)
-            (Bonsai.Map.index_by
-               keyboard
-               ~comparator:(module Analysis.Hand)
-               ~index:(fun (key : Analysis.Key.t) ->
-                 if Analysis.Finger.equal key.finger `i then Some key.hand else None)
-               graph)
-            graph
-            ~f:(fun _ data graph ->
-              let%arr keys = Bonsai.Map.keys data graph in
-              let keys = Set.to_list keys in
-              let visited = ref Analysis.Key.Id.Pair.Set.empty in
-              List.cartesian_product keys keys
-              |> List.filter_map ~f:(fun (k1, k2) ->
-                if Key.Id.equal k1 k2
-                then None
-                else (
-                  let k12 = k1, k2 in
-                  let k21 = k2, k1 in
-                  let curr = !visited in
-                  match Set.mem curr k12, Set.mem curr k21 with
-                  | false, false ->
-                    visited := Set.add !visited k12;
-                    visited := Set.add !visited k21;
-                    Some k12
-                  | _, _ -> None))
-              |> fun l ->
-              List.take l 1
-              |> List.map ~f:(fun x ->
-                print_s ([%sexp_of: Key.Id.t * Key.Id.t] x);
-                x))
-        in
-        List.concat (Map.data x)
-      in
-      let effects =
-        List.map indexes_swaps_for_brute_forcing ~f:(fun swap ->
-          keyboard_inject (Swap swap))
-      in
-      Ui_effect.all_unit effects
-    in
-    Vdom.Node.button
-      ~attrs:[ Vdom.Attr.on_click (fun _event -> effects) ]
-      [ Vdom.Node.text "Brute Force indexes" ]
+    brute_force_indexes_button ~keyboard ~keyboard_inject graph
   in
   let random_swap_vdom =
     let%arr keyboard_inject = keyboard_inject
