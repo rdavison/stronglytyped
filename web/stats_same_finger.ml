82f50c40e2feb2bf03d8df01d05862282f9f400b
Richard Davison <richard.neil.davison@gmail.com>
2025-09-15 07:25:03 +0000

    switch to grid

--- web/stats_same_finger.ml
+++ web/stats_same_finger.ml
@@ -3,87 +3,6 @@ open! Bonsai_web
 open! Bonsai.Let_syntax
 module Form = Bonsai_web_ui_form.With_manual_view
 
-module Style =
-  [%css
-    stylesheet
-      {|
-        .stats-vertical-analysis {
-          padding: 2rem;
-        }
-
-        .stats-vertical-analysis table {
-          border-collapse: collapse;
-          font-family: monospace;
-        }
-
-        .stats-vertical-analysis table th {
-          font-weight: bold;
-          color: black;
-          padding: 0.25rem;
-          border: 1px black solid;
-        }
-
-        .stats-vertical-analysis table td {
-          color: black;
-          padding: 1rem;
-          border: 1px black solid;
-          text-align: center;
-          align-items: center;
-        }
-
-        .stats-vertical-analysis table thead th {
-          border-top: 0;
-        }
-
-        .stats-vertical-analysis table tbody th {
-          border-top: 1;
-        }
-
-        .stats-vertical-analysis table tr:last-child th,
-        .stats-vertical-analysis table tr:last-child td {
-          border-bottom: 0;
-        }
-
-        .stats-vertical-analysis table tr th:first-child,
-        .stats-vertical-analysis table tr td:first-child {
-          border-left: 0;
-        }
-
-        .stats-vertical-analysis table tr th:last-child,
-        .stats-vertical-analysis table tr td:last-child {
-          border-right: 0;
-        }
-
-        .stats-vertical-analysis-inner-table table {
-          border-radius: 0;
-          border-collapse: collapse;
-          border: 0;
-          margin: 0;
-        }
-
-        .stats-vertical-analysis-inner-table table td {
-          border: 1px black solid;
-          padding: 0.5rem;
-          color: black;
-        }
-
-        .stats-vertical-analysis-inner-table table tr:first-child td {
-          border-top: 0;
-        }
-
-        .stats-vertical-analysis-inner-table table tr:last-child td {
-          border-bottom: 0;
-        }
-
-        .stats-vertical-analysis-inner-table table tr td:first-child {
-          border-left: 0;
-        }
-
-        .stats-vertical-analysis-inner-table table tr td:last-child {
-          border-right: 0;
-        }
-|}]
-
 let multiselect graph =
   Form.Elements.Multiselect.set
     (module Analysis.Stats_same_finger.Typed_variant.Packed)
@@ -129,6 +48,26 @@ let render_simple
   Vdom.Node.text (render_measurement packed data)
 ;;
 
+let grid_attr cols =
+  let cols = Int.to_string cols in
+  [%css
+    {|
+      display: grid;
+      grid-template-columns: repeat(%{cols}, 1fr);
+      gap: 4px;
+      background: #222;
+      border-radius: 12px;
+      overflow: hidden;
+
+      & > div {
+        background: #fff;
+        padding: 16px;
+        text-align: center;
+        font-weight: 600;
+      }
+    |}]
+;;
+
 let render_detailed
       (t :
         ((string * float) list, 'total) Analysis.Stats_same_finger.metric
@@ -137,13 +76,13 @@ let render_detailed
   =
   let packed = Analysis.Stats_same_finger.Typed_variant.Packed.pack t in
   Vdom.Node.div
-    ~attrs:[ Style.stats_vertical_analysis_inner_table ]
-    [ Vdom.Node.table
-        (List.map worst ~f:(fun (corpus_key, metric) ->
-           Vdom.Node.tr
-             [ Vdom.Node.td [ Vdom.Node.text corpus_key ]
-             ; Vdom.Node.td [ Vdom.Node.text (render_measurement packed metric) ]
-             ]))
+    ~attrs:[]
+    [ Vdom.Node.div
+        ~attrs:[ grid_attr 2 ]
+        (List.concat_map worst ~f:(fun (corpus_key, metric) ->
+           [ Vdom.Node.div [ Vdom.Node.text corpus_key ]
+           ; Vdom.Node.div [ Vdom.Node.text (render_measurement packed metric) ]
+           ]))
     ]
 ;;
 
@@ -172,7 +111,7 @@ let row
           Analysis.Stats_same_finger.Typed_variant.t)
       ~(metric : ('breakdown, 'total) Analysis.Stats_same_finger.metric Bonsai.t)
       graph
-  : Vdom.Node.t Bonsai.t
+  : Vdom.Node.t list Bonsai.t
   =
   let packed = Analysis.Stats_same_finger.Typed_variant.Packed.pack t in
   let%sub { breakdown; total } = metric in
@@ -182,21 +121,21 @@ let row
       breakdown
       ~f:(fun _key data _graph ->
         let%arr contents = Bonsai.map data ~f:(render_breakdown t) in
-        Vdom.Node.td [ contents ])
+        Vdom.Node.div [ contents ])
       graph
   in
   let total = Bonsai.map total ~f:(render_total packed) in
   let label = label packed in
   let%arr breakdown = breakdown
   and total = total in
-  let header = Vdom.Node.th [ Vdom.Node.text label ] in
+  let header = Vdom.Node.div [ Vdom.Node.text label ] in
   let breakdown =
     List.map Analysis.Hand_finger.all ~f:(fun hand_finger ->
       Map.find breakdown hand_finger
-      |> Option.value ~default:(Vdom.Node.td [ Vdom.Node.none ]))
+      |> Option.value ~default:(Vdom.Node.div [ Vdom.Node.none ]))
   in
-  let total = Vdom.Node.td [ total ] in
-  Vdom.Node.tr ((header :: breakdown) @ [ total ])
+  let total = Vdom.Node.div [ total ] in
+  (header :: breakdown) @ [ total ]
 ;;
 
 let table
@@ -229,17 +168,27 @@ let table
     let to_string = Analysis.Hand_finger.to_string in
     (all |> List.map ~f:to_string) @ [ "Total" ]
   in
-  let scope x = Vdom.Attr.create "scope" x in
-  Vdom.Node.table
-    ~attrs:[]
-    [ Vdom.Node.thead
-        [ Vdom.Node.tr
-            (Vdom.Node.th ~attrs:[] [ (* empty corner cell *) ]
-             :: List.map header ~f:(fun label ->
-               Vdom.Node.th ~attrs:[ scope "col" ] [ Vdom.Node.text label ]))
-        ]
-    ; Vdom.Node.tbody
-        (List.map metrics_order ~f:(fun metric -> Map.find data metric) |> List.filter_opt)
+  Vdom.Node.div
+    ~attrs:
+      [ [%css
+          {|
+            padding: 16px;
+            background: transparent;
+          |}]
+      ]
+    [ Vdom.Node.div (*table*)
+        ~attrs:[ grid_attr 10 ]
+        (let header =
+           Vdom.Node.div (*th*) ~attrs:[] [ (* empty corner cell *) ]
+           :: List.map header ~f:(fun label ->
+             Vdom.Node.div (*th*) ~attrs:[] [ Vdom.Node.text label ])
+         in
+         let rows =
+           List.map metrics_order ~f:(fun metric -> Map.find data metric)
+           |> List.filter_opt
+         in
+         let grid = header :: rows in
+         List.concat grid)
     ]
 ;;
 
@@ -270,7 +219,18 @@ let component ~keyboard ~corpus ~worst_counter graph =
   in
   let vdom =
     let%arr table = table in
-    Vdom.Node.div ~attrs:[ Style.stats_vertical_analysis ] [ table ]
+    Vdom.Node.div
+      ~attrs:
+        [ [%css
+            {|
+              background: #fff;
+              border: 1px solid #e7e9ee;
+              border-radius: 12px;
+              padding: 16px;
+              box-shadow: 0 1px 2px rgba(0,0,0,.04);
+            |}]
+        ]
+      [ table ]
   in
   controls, vdom
 ;;
