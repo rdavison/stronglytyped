456ab15ee73023f0fc4d70a0e11d5ed43f8e9f2e
Richard Davison <richard.neil.davison@gmail.com>
2025-09-15 05:07:19 +0000

    inline css

--- web/key.ml
+++ web/key.ml
@@ -3,51 +3,19 @@ open! Bonsai_web
 open! Bonsai.Let_syntax
 include Analysis.Key
 
-module Style =
-  [%css
-    stylesheet
-      {|
-        .keyboard-key {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          width: var(--keyboard-key-width);
-          height: var(--keyboard-key-height);
-          background-color: var(--keyboard-key-background-color);
-          color: var(--keyboard-key-color);
-          margin: 0.1em;
-          transition-property: color, background-color;
-          transition-timing-function:  cubic-bezier(0.4, 0, 0.2, 1);
-          transition-duration: 300ms;
-          transition-timing-function:  cubic-bezier(0, 0, 0.2, 1);
-          border-radius: 0.5rem;
-          box-shadow: 0 25px 50px -12px rgb(0 0 0 / 0.25);
-          align-items: center;
-          text-align: center;
-        }
-
-        .keyboard-key-overlay {
-          all: inherit;
-          margin: unset;
-        }
-
-        .keyboard-key:hover {
-          background-color: var(--keyboard-key-hover-background-color);
-          color: var(--keyboard-key-hover-color);
-        }
-
-        .keyboard-key-label {
-          display: flex;
-          flex-direction: column;
-        }
-|}]
-
 let render_legend kc =
   match kc with
   | `Alpha _ -> Vdom.Node.text (Analysis.Keycode.to_string_upper kc)
   | `Sym (_, _) ->
+    let css =
+      [%css
+        {|
+          display: flex;
+          flex-direction: column;
+        |}]
+    in
     Vdom.Node.div
-      ~attrs:[ Style.keyboard_key_label ]
+      ~attrs:[ css ]
       [ Vdom.Node.div [ Vdom.Node.text (Analysis.Keycode.to_string_upper kc) ]
       ; Vdom.Node.div [ Vdom.Node.text (Analysis.Keycode.to_string_lower kc) ]
       ]
@@ -64,68 +32,90 @@ let vdom_optional (id : Id.t) (key : t option) =
 let vdom id k corpus_freq_a max_value =
   match k with
   | None ->
-    let keyboard_key_background_color = Tailwind_v3_colors.slate900 in
-    let keyboard_key_color = Tailwind_v3_colors.slate100 in
-    Vdom.Node.div
-      ~attrs:
-        [ Style.keyboard_key
-        ; Style.Variables.set
-            ~keyboard_key_background_color:
-              (keyboard_key_background_color |> Css_gen.Color.to_string_css)
-            ~keyboard_key_color:(keyboard_key_color |> Css_gen.Color.to_string_css)
-            ~keyboard_key_hover_background_color:
-              (Tailwind_v3_colors.slate100 |> Css_gen.Color.to_string_css)
-            ~keyboard_key_hover_color:
-              (Tailwind_v3_colors.slate900 |> Css_gen.Color.to_string_css)
-            ~keyboard_key_width:
-              (Css_gen.Length.to_string_css (`Em_float (4. *. Id.key_width id)))
-            ~keyboard_key_height:(Css_gen.Length.to_string_css (`Em_float 4.))
-            ()
-        ]
-      [ vdom_optional id (Option.map k ~f:fst) ]
+    let css =
+      let background_color = Tailwind_v3_colors.slate900 in
+      let color = Tailwind_v3_colors.slate100 in
+      let width = `Em_float (4. *. Id.key_width id) in
+      let height = `Em_float 4. in
+      [%css
+        {|
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          width: %{width#Css_gen.Length};
+          height: %{height#Css_gen.Length};
+          background-color: %{background_color#Css_gen.Color};
+          color: %{color#Css_gen.Color};
+          margin: 0.1em;
+          border-radius: 0.5rem;
+          box-shadow: 0 25px 50px -12px rgb(0 0 0 / 0.25);
+          align-items: center;
+          text-align: center;
+        |}]
+    in
+    Vdom.Node.div ~attrs:[ css ] [ vdom_optional id (Option.map k ~f:fst) ]
   | Some (k, dnd_attr) ->
     let keyboard_key_background_color = Tailwind_v3_colors.slate900 in
-    let keyboard_key_background_color_overlay =
-      let a, b =
-        match k.kc with
-        | `Alpha a -> Char.lowercase a, Char.uppercase a
-        | `Sym (a, b) -> a, b
-        | _ -> failwith "keycode unsupported"
-      in
-      let freq =
-        (Map.find corpus_freq_a a |> Option.value ~default:0.)
-        +. (Map.find corpus_freq_a b |> Option.value ~default:0.)
-      in
-      let (`RGB (r, g, b)) = Util.Color.convert_hex_to_rgb Tailwind_v3_colors.indigo500 in
-      let a = Percent.of_mult (freq /. max_value) in
-      `RGBA (Css_gen.Color.RGBA.create ~r ~g ~b ~a ())
-    in
     let keyboard_key_color = Tailwind_v3_colors.slate100 in
+    let key_css =
+      let background_color = keyboard_key_background_color in
+      let color = keyboard_key_color in
+      let hover_background_color = Tailwind_v3_colors.slate100 in
+      let hover_color = Tailwind_v3_colors.slate900 in
+      let width = `Em_float (4. *. Id.key_width id) in
+      let height = `Em_float 4. in
+      [%css
+        {|
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          width: %{width#Css_gen.Length};
+          height: %{height#Css_gen.Length};
+          background-color: %{background_color#Css_gen.Color};
+          color: %{color#Css_gen.Color};
+          margin: 0.1em;
+          transition-property: color, background-color;
+          transition-timing-function:  cubic-bezier(0.4, 0, 0.2, 1);
+          transition-duration: 300ms;
+          transition-timing-function:  cubic-bezier(0, 0, 0.2, 1);
+          border-radius: 0.5rem;
+          box-shadow: 0 25px 50px -12px rgb(0 0 0 / 0.25);
+          align-items: center;
+          text-align: center;
+          &:hover {
+            background-color: %{hover_background_color#Css_gen.Color};
+            color: %{hover_color#Css_gen.Color};
+          }
+        |}]
+    in
+    let overlay_css =
+      let background_color =
+        let a, b =
+          match k.kc with
+          | `Alpha a -> Char.lowercase a, Char.uppercase a
+          | `Sym (a, b) -> a, b
+          | _ -> failwith "keycode unsupported"
+        in
+        let freq =
+          (Map.find corpus_freq_a a |> Option.value ~default:0.)
+          +. (Map.find corpus_freq_a b |> Option.value ~default:0.)
+        in
+        let (`RGB (r, g, b)) =
+          Util.Color.convert_hex_to_rgb Tailwind_v3_colors.indigo500
+        in
+        let a = Percent.of_mult (freq /. max_value) in
+        `RGBA (Css_gen.Color.RGBA.create ~r ~g ~b ~a ())
+      in
+      [%css
+        {|
+          all: inherit;
+          margin: unset;
+          background-color: %{background_color#Css_gen.Color};
+        |}]
+    in
     Vdom.Node.div
-      ~attrs:
-        [ Style.keyboard_key
-        ; Style.Variables.set
-            ~keyboard_key_background_color:
-              (keyboard_key_background_color |> Css_gen.Color.to_string_css)
-            ~keyboard_key_color:(keyboard_key_color |> Css_gen.Color.to_string_css)
-            ~keyboard_key_hover_background_color:
-              (Tailwind_v3_colors.slate100 |> Css_gen.Color.to_string_css)
-            ~keyboard_key_hover_color:
-              (Tailwind_v3_colors.slate900 |> Css_gen.Color.to_string_css)
-            ~keyboard_key_width:
-              (Css_gen.Length.to_string_css (`Em_float (4. *. Id.key_width id)))
-            ~keyboard_key_height:(Css_gen.Length.to_string_css (`Em_float 4.))
-            ()
-        ; dnd_attr
-        ]
-      [ Vdom.Node.div
-          ~attrs:
-            [ Style.keyboard_key_overlay
-            ; Vdom.Attr.style
-                (Css_gen.background_color keyboard_key_background_color_overlay)
-            ]
-          [ vdom_optional id (Some k) ]
-      ]
+      ~attrs:[ key_css; dnd_attr ]
+      [ Vdom.Node.div ~attrs:[ overlay_css ] [ vdom_optional id (Some k) ] ]
 ;;
 
 let component id ~keyboard ~corpus_freq_a ~max_value ~dnd _graph =
