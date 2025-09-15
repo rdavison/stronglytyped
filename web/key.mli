891eb64823253a91038a247a4a15fce40485bb4f
Richard Davison <richard.neil.davison@gmail.com>
2025-09-15 06:09:15 +0000

    dragged key color

--- web/key.mli
+++ web/key.mli
@@ -2,9 +2,16 @@ open! Core
 open! Bonsai_web
 include Analysis.Key_intf.S with type t = Analysis.Key.t
 
-val vdom : Id.t -> (t * Vdom.Attr.t) option -> float Char.Map.t -> float -> Vdom.Node.t
-
 val component
+  :  Id.t Bonsai.t
+  -> keyboard:Analysis.Keyboard.t Bonsai.t
+  -> corpus_freq_a:float Char.Map.t Bonsai.t
+  -> max_value:float Bonsai.t
+  -> dnd:(Id.t, Id.t) Bonsai_web_ui_drag_and_drop.t Bonsai.t
+  -> Bonsai.graph
+  -> Vdom.Node.t Bonsai.t
+
+val dragged_component
   :  Id.t Bonsai.t
   -> keyboard:Analysis.Keyboard.t Bonsai.t
   -> corpus_freq_a:float Char.Map.t Bonsai.t
